a8b2fcde5dacd42225c4e16da1c114f9cac422d0
Coralie Pei <pei.coralie@gmail.com>
2022-06-07 16:00:45 +0200

    Recréation des fichiers

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+function Footer() {
+    return (
+      <div className="Footer">
+        <footer className="Footer-txt">
+          <p>NomCompagnie ©</p>
+        </footer>
+      </div>
+    )
+  }
+  
+  export default Footer;
---
+++ src/Header/Header.js
@@ -0,0 +1,22 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="Header">
+        <header className="App-header">
+            <div id="logoHeader">
+
+            </div>
+            <nav>
+                <ul>
+                    <li>Accueil</li>
+                    <li>A propos</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+  );
+}
+
+export default Header;
