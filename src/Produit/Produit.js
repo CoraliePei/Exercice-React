8284f092fc09cb2af2a9c0025595ce2f96a15d78
Coralie Pei <pei.coralie@gmail.com>
2022-06-08 16:35:37 +0200

    Création de fiches produits

--- src/Produit/Produit.js
+++ src/Produit/Produit.js
@@ -1,35 +1,89 @@
 import './Produit.css';
 
 
+// const produits = [
+//     {
+//         name: "Pomme", 
+//         id: 1
+//     },
+//     {
+//         name: "Banane", 
+//         id: 2
+//     },
+//     {
+//         name: "Poire", 
+//         id: 3
+//     },
+//     {
+//         name: "Cerise", 
+//         id: 4
+//     },
+// ]
+
+// export default function Produit() {
+//     return (
+//         <div>
+//             <ul>
+//             {
+//                 produits.map(produit => (
+//                     <li key={produit.id}>{produit.name}</li>
+//                 ))
+//             }
+//             </ul>
+//         </div>
+//     )
+// }
+
+
+// Consigne : créer un tableau produits :
+// image, titre, description, id, prix, avis
+// le composant produit devra afficher une card avec les informations de chaque produit
+// créer un second composant cardProduit qui devra afficher les infos de chaque pdt qui lui sont passés en props
+
 const produits = [
     {
-        name: "Pomme", 
-        id: 1
+      image: 'https://c.shld.net/rpx/i/s/i/spin/10000003/prod_2305917712',
+      id: 1,
+      titre: 'Echo Dot',
+      description: 'Enceinte Bluetooth compacte avec Alexa',
+      prix: '30€',
+      avis: 450,
     },
     {
-        name: "Banane", 
-        id: 2
+      image: 'https://live.staticflickr.com/1701/24066310330_b7bfcd0c0a_n.jpg',
+      id: 2,
+      description: 'Tuner TV usb',
+      titre: 'Fire TV',
+      prix: '70€',
+      avis: 156430,
     },
     {
-        name: "Poire", 
-        id: 3
+      image:
+        'https://www.bhphotovideo.com/images/images2500x2500/fitbit_fb507rgpk_versa_2_health_1501923.jpg',
+      id: 3,
+      description: 'Montre connectée',
+      titre: 'Fitbit Versa ',
+      prix: '120€',
+      avis: 6531,
     },
-    {
-        name: "Cerise", 
-        id: 4
-    },
-]
+  ];
 
 export default function Produit() {
     return (
-        <div>
-            <ul>
-            {
-                produits.map(produit => (
-                    <li key={produit.id}>{produit.name}</li>
-                ))
-            }
-            </ul>
-        </div>
-    )
-}
+    <div className="fichePdt">
+        {   
+            produits.map(produit => (
+                <figure key={produit.id}> 
+                    <img src={produit.image}></img>
+                    <figcaption>
+                        <h3>{produit.titre}</h3>
+                        <p>{produit.description}</p>
+                        <h4>{produit.prix}</h4>
+                        <h5>{produit.avis}</h5>
+                    </figcaption>
+                </figure>
+            ))
+        }
+    </div>
+)}
+
