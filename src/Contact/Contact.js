7cd0fdcc69f40758698b2c7962a074fb345d8ec1
Coralie Pei <pei.coralie@gmail.com>
2022-06-10 13:57:33 +0200

    Ajout du formulaire

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,5 +1,6 @@
 // Commande pour récolter les données entrées dans le formulaire
 import { useState } from "react";
+import './Contact.css';
 
 function Contact() {
 
@@ -7,12 +8,22 @@ function Contact() {
         // Empêche la page de refresh
         event.preventDefault();
         // Mettre des ` quand on intègre une fonction dans une var
-        alert(`Vous êtes ${prenom} ${name}, votre email ${email}`)
+        alert(`Vous êtes ${prenom} ${name}, votre email ${email}. Vous préférez le ${jours}`)
     };
 
+    // Chercher comment tout mettre dans un seul tableau
     const [name, setName] = useState("");
     const [prenom, setPrenom] = useState("");
     const [email, setEmail] = useState("");
+    const [textarea, setTextarea] = useState("Saisissez votre commentaire");
+    const [jours, setJours] = useState("Lundi");
+
+    console.log(textarea);
+    console.log(setJours);
+
+    const handleChange = (event) => {
+        setTextarea(event.target.value);
+    }
 
     return (
         <div className='contact'>
@@ -23,22 +34,31 @@ function Contact() {
                     <input 
                     type="text" 
                     value={name} 
-                    onChange={(element) => setName(element.target.value)} />
+                    onChange={(element) => setName(element.target.value)} required />
                 </label>
                 <label>
                     Prenom :
                     <input 
                     type="text" 
                     value={prenom} 
-                    onChange={(element) => setPrenom(element.target.value)} />
+                    onChange={(element) => setPrenom(element.target.value)} required />
                 </label>
                 <label>
                     Email :
                     <input 
                     type="email" 
                     value={email} 
-                    onChange={(element) => setEmail(element.target.value)} />
+                    onChange={(element) => setEmail(element.target.value)} required />
                 </label>
+                <label>
+                    Jours de la semaine :
+                    <select value={jours} onChange={(element) => setJours(element.target.value)} required >
+                        <option>Lundi</option>
+                        <option>Mardi</option>
+                        <option>Mercredi</option>
+                    </select>
+                </label>
+                <textarea value={textarea} onChange={handleChange} />
                 <input type="submit" />
             </form>
         </div>
